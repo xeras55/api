23800e43e86730085b2e2cf816cba2e57ddff1af
Gerardo Sanchez <114122029+xeras55@users.noreply.github.com>
2023-05-15 20:33:42 -0600

    Completed the security clas and JWT class working

--- api/src/main/java/med/voll/api/controller/AutenticacionController.java
+++ api/src/main/java/med/voll/api/controller/AutenticacionController.java
@@ -6,10 +6,15 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.validation.Valid;
 import med.voll.api.domain.usuarios.DatosAutenticarUsuario;
+import med.voll.api.domain.usuarios.Usuario;
+import med.voll.api.infra.security.DatosJWTToken;
+import med.voll.api.infra.security.TokenService;
 
 @RestController
 @RequestMapping("/login")
@@ -18,12 +23,16 @@ public class AutenticacionController {
   @Autowired
   private AuthenticationManager authenticationManager;
 
+  @Autowired
+  private TokenService tokenService;
+
 
   @PostMapping
-  public ResponseEntity autenticarUsuario(DatosAutenticarUsuario datosAutenticarUsuario){
+  public ResponseEntity autenticarUsuario(@RequestBody @Valid DatosAutenticarUsuario datosAutenticarUsuario){
     UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(datosAutenticarUsuario.login(), datosAutenticarUsuario.clave());
-    authenticationManager.authenticate(token);
-    return ResponseEntity.ok().build();
+    var usuarioAutenticado =  authenticationManager.authenticate(token);
+    var JWTtoken = tokenService.generarToken((Usuario)usuarioAutenticado.getPrincipal());//se valida el usuario que ya se autentico
+    return ResponseEntity.ok(new DatosJWTToken(JWTtoken));
 
 
   }
